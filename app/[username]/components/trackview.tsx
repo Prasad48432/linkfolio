24ce88dd91088ba67b16ba0d52199a6a200294ab
Prasad48432 <prasadreddymar6@gmail.com>
2025-02-22 21:48:46 +0530

    (update) views tracking toggled

--- app/[username]/components/trackview.tsx
+++ app/[username]/components/trackview.tsx
@@ -30,47 +30,47 @@ const TrackView = ({
       console.log("username", username);
       console.log("user_id", profile.id);
 
-      // const { data: existingView } = await supabase
-      //   .from("pageviews")
-      //   .select("id, views, monthly_views")
-      //   .eq("device_id", deviceId)
-      //   .eq("username", username)
-      //   .eq("user_id", profile.id)
-      //   .single();
+      const { data: existingView } = await supabase
+        .from("pageviews")
+        .select("id, views, monthly_views")
+        .eq("device_id", deviceId)
+        .eq("username", username)
+        .eq("user_id", profile.id)
+        .single();
 
-      // // Format month as "MMM-YY" (e.g., "Jan-25")
-      // const currentMonth = new Date()
-      //   .toLocaleString("en-US", {
-      //     month: "short",
-      //     year: "2-digit",
-      //   })
-      //   .replace(" ", "-");
+      // Format month as "MMM-YY" (e.g., "Jan-25")
+      const currentMonth = new Date()
+        .toLocaleString("en-US", {
+          month: "short",
+          year: "2-digit",
+        })
+        .replace(" ", "-");
 
-      // console.log(existingView);
-      // console.log(currentMonth);
+      console.log(existingView);
+      console.log(currentMonth);
 
-      // if (existingView) {
-      //   let monthlyViews = existingView.monthly_views || {};
-      //   monthlyViews[currentMonth] = (monthlyViews[currentMonth] || 0) + 1;
-      //   await supabase
-      //     .from("pageviews")
-      //     .update({
-      //       views: existingView.views + 1,
-      //       monthly_views: monthlyViews,
-      //     })
-      //     .eq("id", existingView.id);
-      // } else {
-      //   const newMonthlyViews = { [currentMonth]: 1 };
-      //   await supabase.from("pageviews").insert([
-      //     {
-      //       device_id: deviceId,
-      //       views: 1,
-      //       username: username,
-      //       user_id: profile.id,
-      //       monthly_views: newMonthlyViews,
-      //     },
-      //   ]);
-      // }
+      if (existingView) {
+        let monthlyViews = existingView.monthly_views || {};
+        monthlyViews[currentMonth] = (monthlyViews[currentMonth] || 0) + 1;
+        await supabase
+          .from("pageviews")
+          .update({
+            views: existingView.views + 1,
+            monthly_views: monthlyViews,
+          })
+          .eq("id", existingView.id);
+      } else {
+        const newMonthlyViews = { [currentMonth]: 1 };
+        await supabase.from("pageviews").insert([
+          {
+            device_id: deviceId,
+            views: 1,
+            username: username,
+            user_id: profile.id,
+            monthly_views: newMonthlyViews,
+          },
+        ]);
+      }
     };
 
     trackView();
